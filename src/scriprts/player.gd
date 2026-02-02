1ddcd08446abe235ede9aae9759536312b65b68d
jinzhehao <jinzhehao@souche.com>
2026-02-02 15:45:40 +0800

    feat: 增加人物跳跃功能;人物走路速度缓降功能

--- src/scriprts/player.gd
+++ src/scriprts/player.gd
@@ -8,35 +8,50 @@ enum PlayStatus {
 	RUN,
 }
 
-@export var SPEED = 150
-@export var JUMP_VELOCITY = -300
-
 @export var state: Dictionary = {
-	"status": PlayStatus.IDLE,
+	"current_status": PlayStatus.IDLE,
+	"run_speed": 100,
+	"run_reduce_speed": 10,
+	"jump_speed": 300,
 }
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	handleJump()
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("left", "right")
-	if direction:
-		velocity.x = direction * SPEED
+	if is_on_floor():
+		if Input.is_action_just_pressed("jump"):
+			velocity.y = - state.get("jump_speed")
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.y += get_gravity().y * delta
+
+	var direction = Input.get_axis("left", "right")
+
+	if direction:
+		velocity.x = direction * state.get("run_speed") * 100 * delta
+		if direction > 0:
+				animated_sprite.flip_h = false
+		else:
+			animated_sprite.flip_h = true
+	else:
+		velocity.x = move_toward(velocity.x, 0, state.get("run_reduce_speed") * 100 * delta)
 
 	move_and_slide()
 
-func handleJump() -> void:
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-		pass
+
+	# handleJump()
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	# var direction := Input.get_axis("left", "right")
+
+	# if direction:
+	# 	velocity.x = direction * SPEED * delta
+	# else:
+	# 	velocity.x = move_toward(velocity.x, 0, SPEED * delta)
+
+
+# func handleJump() -> void:
+
 
 func on_attack_by_slime() -> void:
 	animated_sprite.play("dead")
