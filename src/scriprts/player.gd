6f9ac05fe812076ae39a8fd5652377856395d686
jinzhehao <jinzhehao@souche.com>
2026-02-02 14:26:41 +0800

    feat: 增加状态

--- src/scriprts/player.gd
+++ src/scriprts/player.gd
@@ -1,20 +1,27 @@
 extends CharacterBody2D
 
+enum PlayStatus {
+	DEAD,
+	IDLE,
+	JUMP,
+	ROLL,
+	RUN,
+}
 
 @export var SPEED = 150
 @export var JUMP_VELOCITY = -300
 
+@export var state: Dictionary = {
+	"status": PlayStatus.IDLE,
+}
+
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		animated_sprite.play("jump")
-		velocity.y = JUMP_VELOCITY
+	handleJump()
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -26,7 +33,10 @@ func _physics_process(delta: float) -> void:
 
 	move_and_slide()
 
+func handleJump() -> void:
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		pass
 
 func on_attack_by_slime() -> void:
-
 	animated_sprite.play("dead")
