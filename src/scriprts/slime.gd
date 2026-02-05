a2db7adc156240b6043417ffe6758d96354a0422
jsjzh <kimimi_king@163.com>
2026-02-05 23:50:37 +0800

    feat: 地图设计准备

--- src/scriprts/slime.gd
+++ src/scriprts/slime.gd
@@ -4,3 +4,9 @@ func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		if body.has_method("on_attack_by_slime"):
 			body.on_attack_by_slime()
+
+
+func _on_area_entered(area: Area2D) -> void:
+	if area.is_in_group("arrow"):
+		queue_free()
+		area.queue_free()
